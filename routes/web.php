360d483351609c43d089a858d8bab298719f2f75
Salsabila7-3 <shofiyyatus07@gmail.com>
2024-09-10 19:29:37 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('program', function () {
-    $program ['php' , 'java', 'c', 'javascript' , 'dart'];
+    $program = ['php' , 'java', 'c', 'javascript', 'dart'];
     return $program;
 });
